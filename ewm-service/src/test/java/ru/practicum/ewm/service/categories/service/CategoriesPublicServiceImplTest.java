2b748f8f014827d0cc7b4994f5011c0d4b11a2c2
Alex Alexeyev <aasmc@yandex.ru>
2023-09-06 21:04:33 +0300

    feat: complete tests for CategoriesPublicServiceImpl

--- ewm-service/src/test/java/ru/practicum/ewm/service/categories/service/CategoriesPublicServiceImplTest.java
+++ ewm-service/src/test/java/ru/practicum/ewm/service/categories/service/CategoriesPublicServiceImplTest.java
@@ -9,6 +9,11 @@ import ru.practicum.ewm.service.categories.dto.CategoryDto;
 import ru.practicum.ewm.service.categories.repository.CategoriesRepository;
 import ru.practicum.ewm.service.error.EwmServiceException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ru.practicum.ewm.service.error.ErrorConstants.*;
@@ -20,6 +25,31 @@ class CategoriesPublicServiceImplTest extends BaseIntegTest {
 
     private final CategoriesPublicServiceImpl publicService;
     private final CategoriesRepository repository;
+
+    @Test
+    void getCategories_whenNoCategories_returnsEmptyList() {
+        List<CategoryDto> categories = publicService.getCategories(0, 10);
+        assertThat(categories).isEmpty();
+    }
+
+    @Test
+    void getCategories_whenAllOk_returnsCorrectListWithCorrectOffset() {
+        List<Category> categories = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            categories.add(saveCategory(CATEGORY_NAME + i));
+        }
+        List<CategoryDto> result = publicService.getCategories(3, 5)
+                .stream()
+                .sorted(Comparator.comparing(CategoryDto::getName))
+                .collect(Collectors.toList());
+
+        assertThat(result).hasSize(5);
+        assertThat(result.get(0).getName()).isEqualTo(categories.get(3).getName());
+        assertThat(result.get(1).getName()).isEqualTo(categories.get(4).getName());
+        assertThat(result.get(2).getName()).isEqualTo(categories.get(5).getName());
+        assertThat(result.get(3).getName()).isEqualTo(categories.get(6).getName());
+        assertThat(result.get(4).getName()).isEqualTo(categories.get(7).getName());
+    }
 
     @Test
     void getCategory_whenAllOk_returnsCategory() {
